7e06c38464b427be23e538f286ab5a8774730054
Dmytro1996 <dmytrodanylov@ukr.net>
2019-08-15 18:44:08 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -1,32 +1,31 @@
-
-document.querySelector('.addTextToFirstArticle').addEventListener('click',increaseUsefulness);
+document.querySelector('.addTextToUsefulnessArticle').addEventListener('click',increaseUsefulness);
 function increaseUsefulness() {
     fetch('addData.html')
     .then(response=>response.text())
     .then(html=>document.querySelector('.firstArticle').innerHTML=html);
-    document.querySelector('.addTextToFirstArticle').value='Показати менше';
-    document.querySelector('.addTextToFirstArticle').removeEventListener('click',increaseUsefulness);
-    document.querySelector('.addTextToFirstArticle').addEventListener('click',decreaseUsefulness);
+    document.querySelector('.addTextToUsefulnessArticle').value='Показати менше';
+    document.querySelector('.addTextToUsefulnessArticle').removeEventListener('click',increaseUsefulness);
+    document.querySelector('.addTextToUsefulnessArticle').addEventListener('click',decreaseUsefulness);
 }
 function decreaseUsefulness() {
    document.querySelector('.firstArticle').innerHTML='';
-   document.querySelector('.addTextToFirstArticle').value='Показати більше';
-   document.querySelector('.addTextToFirstArticle').removeEventListener('click',decreaseUsefulness);
-   document.querySelector('.addTextToFirstArticle').addEventListener('click',increaseUsefulness);
+   document.querySelector('.addTextToUsefulnessArticle').value='Показати більше';
+   document.querySelector('.addTextToUsefulnessArticle').removeEventListener('click',decreaseUsefulness);
+   document.querySelector('.addTextToUsefulnessArticle').addEventListener('click',increaseUsefulness);
 }
-document.querySelector('.addTextToSecondArticle').addEventListener('click',increaseChoice);
+document.querySelector('.addTextToChoiceArticle').addEventListener('click',increaseChoice);
 function increaseChoice() {
     fetch('addDataToChoice.html')
     .then(response=>response.text())
     .then(html=>document.querySelector('.secondArticle').innerHTML=html);
-    document.querySelector('.addTextToSecondArticle').value='Показати менше';
-    document.querySelector('.addTextToSecondArticle').removeEventListener('click',increaseChoice);
-    document.querySelector('.addTextToSecondArticle').addEventListener('click',decreaseChoice);
+    document.querySelector('.addTextToChoiceArticle').value='Показати менше';
+    document.querySelector('.addTextToChoiceArticle').removeEventListener('click',increaseChoice);
+    document.querySelector('.addTextToChoiceArticle').addEventListener('click',decreaseChoice);
 }
 function decreaseChoice() {
    document.querySelector('.secondArticle').innerHTML='';
-   document.querySelector('.addTextToSecondArticle').value='Показати більше';
-   document.querySelector('.addTextToSecondArticle').removeEventListener('click',decreaseChoice);
-   document.querySelector('.addTextToSecondArticle').addEventListener('click',increaseChoice);
+   document.querySelector('.addTextToChoiceArticle').value='Показати більше';
+   document.querySelector('.addTextToChoiceArticle').removeEventListener('click',decreaseChoice);
+   document.querySelector('.addTextToChoiceArticle').addEventListener('click',increaseChoice);
 }
 
