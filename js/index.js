d8e5192b715b2ca53d8a58d26a5b1462e3840996
Dmytro1996 <dmytrodanylov@ukr.net>
2019-08-15 22:41:45 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -28,4 +28,37 @@ function decreaseChoice() {
    document.querySelector('.addTextToChoiceArticle').removeEventListener('click',decreaseChoice);
    document.querySelector('.addTextToChoiceArticle').addEventListener('click',increaseChoice);
 }
+const addCommentbtn=document.querySelector('.addComment');
+addCommentbtn.addEventListener('click',addComment);
+const text=document.querySelector('#commentText');
+const name=document.querySelector('#commenterName');
+function addComment() {
+    if(text.value!='' && name.value!='') {
+    localStorage[localStorage.length+1]=name.value+':'+text.value;
+    } else{
+        alert('Заповніть всі поля');
+    }
+    text.value='';
+    name.value='';
+    }
+const showCommentsBtn=document.querySelector('.showComments');
+showCommentsBtn.addEventListener('click',show);
+function show() {
+ for(i=1;i<=localStorage.length;i++) {
+ const para=document.createElement('p');
+ para.innerHTML=localStorage[i] ;
+ document.querySelector('#comments').appendChild(para);
+ }
+ showCommentsBtn.value='Сховати коментарі';
+ showCommentsBtn.removeEventListener('click',show);
+ showCommentsBtn.addEventListener('click',hide);
+}
+function hide() {
+    document.querySelector('#comments').innerHTML='';
+    showCommentsBtn.value='Показати коментарі';
+    showCommentsBtn.removeEventListener('click',hide);
+    showCommentsBtn.addEventListener('click',show);
+
+}
+
 
