b18bf5cd07f1c2b2a10f0e07c0f86ab4be95cfee
dmytr <dmytr@DESKTOP-N8AF4RV>
2020-12-11 19:56:46 +0200

    created BicycleWorldApplication,WebSecurityConfigurer,WebAuthenticationProvider

---
+++ bicycleworld/src/main/java/com/mycompany/bicycleworld/WebAuthenticationProvider.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bicycleworld;
+
+import com.mycompany.bicycleworld.details.Role;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author dmytr
+ */
+@Component
+public class WebAuthenticationProvider implements AuthenticationProvider {
+    @Autowired
+    private UserDetailsService userDetailsService;    
+    
+    @Autowired
+    private BCryptPasswordEncoder passEncoder; 
+    Logger logger=LoggerFactory.getLogger(WebAuthenticationProvider.class);
+    
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException{        
+        String username=authentication.getName();        
+        String password=authentication.getCredentials().toString();
+        logger.info(username+" "+password);
+        UserDetails userDetails=userDetailsService.loadUserByUsername(username);
+        //logger.info(userDetails==null);
+        Role role=(Role)userDetails.getAuthorities().toArray()[0];        
+        if(userDetails!=null && passEncoder.matches(password,userDetails.getPassword())){            
+            return new UsernamePasswordAuthenticationToken(userDetails,
+                    userDetails.getPassword(),userDetails.getAuthorities());
+        }
+        return null;
+    }
+    
+    @Override
+    public boolean supports(Class<?> authentication){
+        return true;
+    }
+}
---
+++ bicycleworld/src/main/java/com/mycompany/bicycleworld/WebSecurityConfigurer.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bicycleworld;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ *
+ * @author dmytr
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter  {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{       
+        http.authorizeRequests().antMatchers("/users/create","/page/**","/css/**",
+                "/javascript/**","/images/**").permitAll().anyRequest().authenticated().and().formLogin()
+                .loginPage("/").loginProcessingUrl("/login")
+                .defaultSuccessUrl("/index").failureUrl("/login-form?error=true")
+                .permitAll().and().logout().logoutUrl("/logout")
+                .logoutSuccessUrl("/index").deleteCookies("JSESSIONID");       
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth, AuthenticationProvider provider) throws Exception{        
+        auth.authenticationProvider(provider);
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
