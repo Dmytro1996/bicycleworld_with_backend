e41e90476eeaeb5b360874ca7f86f30db5709ba9
dmytr <dmytr@DESKTOP-N8AF4RV>
2020-12-11 19:59:06 +0200

    created application.properties and data.sql

---
+++ bicycleworld/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  dmytr
+ * Created: 9 груд. 2020
+ */
+INSERT INTO Articles(id,direction) VALUES(1,"C:\\Users\\dmytr\\OneDrive\\Documents\\NetBeansProjects\\bicycleworld\\bicycleworld\\src\\main\\resources\\articles\\Article about choosing a bicycle.txt");
+INSERT INTO Articles(id,direction) VALUES(2,"C:\\Users\\dmytr\\OneDrive\\Documents\\NetBeansProjects\\bicycleworld\\bicycleworld\\src\\main\\resources\\articles\\Article about usefullness of riding a bicycle.txt");
+INSERT INTO Users(id,full_name,email,password) VALUES(1,"John","john@mail.com","$2y$12$HyKB5hJ9pLLAGQ4ff8GJB.pLWGoF.7Kout7v2w4E0.JswWKCRtwc.");
+INSERT INTO Users(id,full_name,email,password) VALUES(2,"George","george@mail.com","$2y$12$VgTPP8rl2yYPNC0eVOo5SO4845lCChJ7u8319n2CxT4vMZqFbzj2W");
+INSERT INTO Users(id,full_name,email,password) VALUES(3,"Sam","sam@mail.com","$2y$12$u3ReBjbei.RMzq6pY6c1XuYWCRBmlsFH77g/CkN8CJWVZeVULQKtS ");
+
+INSERT INTO Comments(id,comment,user_id,article_id) VALUES(1,"Now I know what bicycle to choose",1,1);
+INSERT INTO Comments(id,comment,user_id,article_id) VALUES(2,"Very useful article",2,1);
+INSERT INTO Comments(id,comment,user_id,article_id) VALUES(3,"It helped me a lot",3,1);
+INSERT INTO Comments(id,comment,user_id,article_id) VALUES(4,"Now I will ride a bike more often",1,2);
+INSERT INTO Comments(id,comment,user_id,article_id) VALUES(5,"Interesting article",2,2);
