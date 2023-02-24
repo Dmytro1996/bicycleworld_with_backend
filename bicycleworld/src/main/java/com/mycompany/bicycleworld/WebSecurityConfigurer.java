e297993a8067b94e28c0e40560710da44b53fc2f
dmytr <dmytr@DESKTOP-N8AF4RV>
2023-02-24 22:26:54 +0200

    added oauth2

--- bicycleworld/src/main/java/com/mycompany/bicycleworld/WebSecurityConfigurer.java
+++ bicycleworld/src/main/java/com/mycompany/bicycleworld/WebSecurityConfigurer.java
@@ -5,6 +5,13 @@
  */
 package com.mycompany.bicycleworld;
 
+import com.mycompany.bicycleworld.model.AuthProvider;
+import com.mycompany.bicycleworld.model.User;
+import com.mycompany.bicycleworld.service.UserService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +20,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 /**
  *
  * @author dmytr
@@ -22,14 +31,37 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter  {
+    
+    @Autowired
+    private UserService userService;
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception{       
-        http.authorizeRequests().antMatchers("/users/create","/page/**","/css/**",
-                "/javascript/**","/images/**").permitAll().anyRequest().authenticated().and().formLogin()
+        http.csrf().disable().authorizeRequests().antMatchers("/users/create","/page/**","/css/**",
+                "/javascript/**","/images/**","/login","/index","/").permitAll().anyRequest().authenticated().and().formLogin()
                 .loginPage("/").loginProcessingUrl("/login")
                 .defaultSuccessUrl("/index").failureUrl("/error?userNotFound=true")
                 .permitAll().and().logout().logoutUrl("/logout")
-                .logoutSuccessUrl("/index").deleteCookies("JSESSIONID");       
+                .logoutSuccessUrl("/index").deleteCookies("JSESSIONID").and().oauth2Login()
+                .successHandler(new AuthenticationSuccessHandler(){
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest hsr, 
+                            HttpServletResponse hsr1, Authentication a) 
+                            throws IOException, ServletException {
+                        OAuth2User oauthUser=(OAuth2User)a.getPrincipal();
+                        String email=oauthUser.getAttribute("email");                        
+                        if(userService.getAll().stream().filter(u->u.getEmail().equals(email)).count()==0){
+                            User user=new User();
+                            user.setFullName(oauthUser.getAttributes().get("name").toString());
+                            user.setEmail(email);
+                            user.setPassword("default_pass");
+                            user.setAuthProvider(AuthProvider.GOOGLE);
+                            userService.create(user);
+                        }
+                        hsr1.sendRedirect("/index");
+                    }
+                
+                });       
     }
     
     @Autowired
