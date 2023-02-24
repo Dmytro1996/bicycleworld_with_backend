c3801a4e5e86dc988afa645fc90baf75505f605e
dmytr <dmytr@DESKTOP-N8AF4RV>
2023-02-24 22:29:35 +0200

    added authentication principal to home() method and more logs

--- bicycleworld/src/main/java/com/mycompany/bicycleworld/controllers/HomeController.java
+++ bicycleworld/src/main/java/com/mycompany/bicycleworld/controllers/HomeController.java
@@ -12,10 +12,14 @@ import com.mycompany.bicycleworld.service.HomeService;
 import com.mycompany.bicycleworld.service.UserService;
 import java.io.File;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,11 +49,17 @@ public class HomeController {
     
     
     @RequestMapping({"/","/index"})
-    public String home(Model model){
-        logger.info("Inside controller");      
+    public String home(Model model, @AuthenticationPrincipal OAuth2User principal){
+        logger.info("Inside controller");        
+        System.out.println("Principal is null: "+(principal==null));
+        System.out.println(principal==null?null:principal.getAttributes().get("given_name"));
         List<Article> articles=articleService.getAll().stream().limit(10).collect(Collectors.toList());              
         for(Article a:articles){            
             a.setText(homeService.getArticle(new File(a.getDirection())));
+        }
+        model.addAttribute("principalType",principal==null?"custom":"google");
+        if(principal!=null){
+            model.addAttribute("principalGivenName",principal.getAttribute("given_name"));
         }
         model.addAttribute("hasPreviousArticles",currentPage>1);
         model.addAttribute("hasNextArticles",currentPage*10<articleService.getAll().size());
